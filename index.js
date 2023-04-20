14adb7374b0b35969a8e687ce2605a8a199d5bc7
chloe hausser <chloerae018@gmail.com>
2023-04-20 18:59:01 -0400

    what even

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 "use strict";
 const loc1 = '2712 W Main St, Waynesboro, VA 22980'; //Amazon WCH2
 const loc2 = '1656 Old Greensboro Rd, Kernersville, NC 27284'; //GSO1 - Amazon Fulfillment Center
-const KEY = 'WOOm9BsTrflfVpN5PJw9sLtj5DQsfGgB';
+// sniped this key from mapquests example sourcecode..
+// only using it till their support gets my keys working
+const KEY = 'ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7';
 const url = 'https://www.mapquestapi.com/directions/v2/route';
-/* function getTravelTime(locA: string, locB: string){
-    fetch(`${url}?key=${KEY}&from=${locA}&to=${locB}`).then(function(response) {        console.log('Success!');
-        //return 0;
-    }).then(function(data) {
-        console.log(data);
-        //return 0;
-    }).catch(function (err) {
-        console.warn('Something went wrong.', err);
-        //return 0;
+async function getTravelInfo(locA, locB) {
+    var info;
+    fetch(`${url}?key=${KEY}&from=${locA}&to=${locB}`)
+        .then(res => res.json())
+        .then(data => {
+        info = [data["realTime"], data["distance"], data["formattedTime"]];
+        return info;
     });
-} */
-//getTravelTime(loc1, loc2);
-fetch(`https://www.mapquestapi.com/directions/v2/route?key=WOOm9BsTrflfVpN5PJw9sLtj5DQsfGgB&from=Clarendon+Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA`).then(function (response) {
-    console.log('Success!');
-    //return 0;
-}).then(function (data) {
-    console.log(data);
-    //return 0;
-}).catch(function (err) {
-    console.warn('Something went wrong.', err);
-    //return 0;
-});
+}
+async function main() {
+    let result = await getTravelInfo(loc1, loc2);
+}
+main().catch(console.log);
